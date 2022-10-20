fdfb65bb8fa4b2021f9f787491b094c149bc3532
taoshumin_vendor <tsmidx@gmail.com>
2022-10-20 21:08:23 +0800

    init

---
+++ cmd/main.go
@@ -0,0 +1,42 @@
+/*
+Copyright 2022 The Workpieces LLC.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"github.com/syncthing/notify"
+	"log"
+)
+
+func main() {
+	c := make(chan notify.EventInfo, 1)
+
+	// Set up a watchpoint listening on events within current working directory.
+	// Dispatch each create and remove events separately to c.
+	// Y:\1006100210002\Info
+	if err := notify.Watch(".", c, notify.All); err != nil {
+		log.Fatal(err)
+	}
+	defer notify.Stop(c)
+
+	for {
+		select {
+		case ei := <-c:
+			log.Println("Got event:", ei)
+
+		}
+	}
+}
